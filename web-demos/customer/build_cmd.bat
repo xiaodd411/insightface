5ecae83ffd0f205947acc168175ca692ca2fd5fe
xiaoqi <hehuo411@qq.com>
2025-02-11 10:17:47 +0800

    build(web-demos/customer): 添加 Windows 和 Linux 打包脚本- 新增 build_cmd.bat 文件，包含 Windows 系统下的打包命令 - 新增 Dockerfile 文件，用于在 Linux 系统下构建和打包项目 - 提供了两种打包方式：单文件打包和目录打包，以满足不同需求

---
+++ web-demos/customer/build_cmd.bat
@@ -0,0 +1,7 @@
+
+-- 使用打包为单个文件的话再程序启动时会有解压文件到临时目录的操作,会导致程序运行耗时教程
+pyinstaller --onefile --collect-binaries=onnx --hidden-import=onnx --hidden-import=onnx_cpp2py_export --add-binary "C:\Users\xq\Desktop\project\insightface\venv1\Lib\site-packages\onnx\onnx_cpp2py_export.cp311-win_amd64.pyd;onnx" main.py
+
+
+--  使用目录打包的话会减少程序执行时所耗时间,但是打包后的文件会变大
+pyinstaller --onedir --collect-binaries=onnx --hidden-import=onnx --hidden-import=onnx_cpp2py_export --add-binary "C:\Users\xq\Desktop\project\insightface\venv1\Lib\site-packages\onnx\onnx_cpp2py_export.cp311-win_amd64.pyd;onnx" main.py
